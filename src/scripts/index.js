3e0dbff16eb17ac1a053f03fa1fbad5c60e55995
Kauê Lima <56000639+KaueACLima@users.noreply.github.com>
2025-01-24 22:14:13 -0300

    Update scripts/index.js

---
+++ src/scripts/index.js
@@ -0,0 +1,52 @@
+adocument.getElementById('btn-search').addEventListener('click', () => {
+  const userName = document.getElementById('input-search').value
+  getUserProfile(userName)
+})
+
+document.getElementById('input-search').addEventListener('keyup', (e) => {
+  const userName = e.target.value
+  const key = e.which || e.keyCode
+  const isEnterKeyPressed = key === 13
+
+  if (isEnterKeyPressed) {
+    getUserProfile(userName)
+  }
+})
+
+async function user(userName) {
+  const response = await fetch(`https://api.github.com/users/${userName}`)
+  return await response.json()
+}
+
+async function repos(userName) {
+  const response = await fetch(`https://api.github.com/users/${userName}/repos`)
+  return await response.json()
+}
+
+function getUserProfile(userName){
+
+
+
+  user().then(userData => {
+    let userInfo = `<img src="${userData.avatar_url}" alt="Foto de perfil do usúario"/>
+                    <div class="data">
+                        <h1>${userData.name ?? 'Não possui nome cadastrado 😅'}</h1>
+                        <p>${userData.bio ?? 'Não possui bio cadastrado 😅'}</p>
+                    </div>`
+    document.querySelector('.profile-data').innerHTML = userInfo
+  })
+}
+
+function getUserRepositories(userName){
+    repos(userName).then(reposData => {
+      let repositoriesItens = ""
+      reposData.forEach(repo => {
+        repositoriesItens += `<li><a href="${repo.html_url} target="_blank">${repo.name}</a>"</li>`
+      })
+
+      document.querySelector('.profile-data'),innerHTML += `<div class="repositories section">
+                                                              <h2>Repositórios</h2>
+                                                              <ul>${repositoriesItens}</ul>
+                                                            </div>`
+    })
+}
