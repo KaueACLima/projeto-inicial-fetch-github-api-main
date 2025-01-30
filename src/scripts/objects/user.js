90e14f7e9a1f0317a8999369f14452c2d1fa7072
Kauê Lima <56000639+KaueACLima@users.noreply.github.com>
2025-01-30 17:35:58 -0300

    Update scripts/index.js

--- src/scripts/objects/user.js
+++ src/scripts/objects/user.js
@@ -1,17 +1,26 @@
 const user = {
-  avatarUrl: '',
-  name: '',
-  bio: '',
-  userName: [],
-  setInfo(gitHubUser) {
-    this.avaterUrl = gitHubUser.avartar_url
-    this.name = gitHubUser.name
-    this.bio = gitHubUser.bio
-    this.userName = gitHubUser.login
-  },
-  setRepositorioes(repositories) {
-    this.repositories = repositories
-  }
+    avatarUrl: '',
+    name: '',
+    bio: '',
+    userName: '',
+    followers: '',
+    following: '',
+    repositories: [],
+    events: [],
+    setInfo(gitHubUser){
+        this.avatarUrl = gitHubUser.avatar_url
+        this.name = gitHubUser.name
+        this.bio = gitHubUser.bio
+        this.userName = gitHubUser.login
+        this.followers = gitHubUser.followers
+        this.following = gitHubUser.following
+    },
+    setRepositories(repositories){
+        this.repositories = repositories
+    },
+    setEvents(events){
+        this.events = events
+    }
 }
 
 export { user }
--- src/scripts/sevices/repositories.js
+++
@@ -1,8 +0,0 @@
-import { baseUrl, repositoriesQuantity } from "./src/scripts/variables.js";
-
-async function repositories(userName) {
-  const response = await fetch(`${baseUrl}/${userName}/repos?per_pages=${repositoriesQuantity}`)
-  return await response.json()
-}
-
-export { repositories }
